ecc32802be11acb51f89b8514daa53815b6ff47e
ShreyVaghela <shrey.r.vaghela@gmail.com>
2020-02-28 21:30:09 -0400

    Printing request id,time,N,result

--- factorial.py
+++ factorial.py
@@ -1,4 +1,10 @@
 import math
+import time
 f=open("Input.txt","r")
+id=1
 for line in f:
-    print(math.factorial(int(line)))
+	start=time.clock()	
+	val=math.factorial(int(line))    
+	Time=(time.clock()-start)*1e6
+	print "Request Id:%d, Time:%.3f,N:%d,result:%d\n"%(id,Time,int(line),val)
+	id+=1
--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,26 @@
+import time
 f=open("Input.txt","r")
+id=1
+
 for line in f:
+	start=time.clock()
 	a=0
 	b=1
 	num=0
 	if int(line)==1:
-		print(a)
+		Time=(time.clock()-start)*1e6
+		print "Request Id:%d, Time:%.3f,N:%d,result:%d\n"%(id,Time,int(line),a)
+		id+=1
 	else:
-		print "\n"
+		arr=[]
 		while num<int(line):
-			print a,
+			arr.append(a)
 			temp=a+b
 			a=b
 			b=temp
 			num+=1
+		Time=(time.clock()-start)*1e6
+		print "Request Id:%d, Time:%.3f,N:%d,result:%s\n"%(id,Time,int(line),arr)
+		id+=1
     
 
